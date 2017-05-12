4204ca429bdcb46f8ba3220f1197f0ca782e9262
owacoder <owacoder@gmail.com>
2017-05-12 10:53:13 -0400

    Update general.h

--- general.h
+++ general.h
@@ -4,8 +4,10 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#if !defined(max) && !defined(min)
 size_t max(size_t a, size_t b);
 size_t min(size_t a, size_t b);
+#endif
 int ispow2(size_t n);
 int ispow2x(uintmax_t n);
 int lg2(size_t n);
